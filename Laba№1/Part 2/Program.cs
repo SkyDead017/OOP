bfdeb730794d85264f34fd2436b45289dcf21562
SkyDead017 <109245608+SkyDead017@users.noreply.github.com>
2024-10-11 09:29:11 +0500

    Add files via upload

---
+++ Laba№1/Part 2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_1_OOP_Part_2_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float x, y;
+            Console.WriteLine("Проверка принадлежности точки");
+            Console.WriteLine("Введите координату x: ");
+            while (!float.TryParse(Console.ReadLine(), out x))
+            {
+                Console.WriteLine("ОШИБКА! Введите число!");
+            }
+            Console.WriteLine("Введите координату y: ");
+            while (!float.TryParse(Console.ReadLine(), out y))
+            {
+                Console.WriteLine("ОШИБКА! Введите число!");
+            }
+            bool flag=false;
+            if(y<=x+2 && y>=0 && y <= -x + 2)
+            {
+                flag = true;
+            }
+            if (flag)
+            {
+                Console.WriteLine("Точка принадлежит заштрихованной области");
+            }
+            else
+            {
+                Console.WriteLine("Точка НЕ принадлежит заштрихованной области");
+            }
+        }
+    }
+}
