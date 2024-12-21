6538627bf4319c122a4c43c8f83682acec66dfc1
SkyDead017 <109245608+SkyDead017@users.noreply.github.com>
2024-12-21 11:11:57 +0500

    Add files via upload

---
+++ Laba№11/Program.cs
@@ -0,0 +1,392 @@
+﻿using System.Diagnostics;
+using ProductionLib;
+
+namespace lab11
+{
+    internal class Program
+    {
+        static Stopwatch timer = new Stopwatch();
+        static void Main(string[] args)
+        {
+            TestCollections tests = new TestCollections(1000);
+
+            Factory first = new Factory(
+                tests.factQueue.Peek().Type,
+                tests.factQueue.Peek().CountProd,
+                tests.factQueue.Peek().CountDepartments);
+        
+            Factory last = tests.GetLastTest();
+            Factory lastCopy = new Factory(
+                last.Type,
+                last.CountProd,
+                last.CountDepartments);
+
+            Factory middle = tests.GetMiddleTest();
+            Factory middleCopy = new Factory(
+                middle.Type,
+                middle.CountProd,
+                middle.CountDepartments);
+
+            Factory custom = new Factory("ТИП", 99999, 999);
+            
+            Console.WriteLine("Queue<Factory>: ");
+            TimeQueueTestLookup(tests, first, lastCopy, middleCopy, custom);
+            Console.WriteLine();
+
+            Console.WriteLine("Queue<string>: ");
+            TimeQueueStringLookup(tests, first, lastCopy, middleCopy, custom);
+            Console.WriteLine();
+
+            Console.WriteLine("Dictionary<Production, Factory> (ContainsKey): ");
+            TimeDictionaryProdKeyLookup(tests, first, lastCopy, middleCopy, custom);
+            Console.WriteLine();
+
+            Console.WriteLine("Dictionary<string, Factory> (ContainsKey): ");
+            TimeDictionaryStringKeyLookup(tests, first, lastCopy, middleCopy, custom);
+            Console.WriteLine();
+
+            Console.WriteLine("Dictionary<Production, Factory> (ContainsValue): ");
+            TimeDictionaryProdValueLookup(tests, first, lastCopy, middleCopy, custom);
+            Console.WriteLine();
+
+            Console.WriteLine("Dictionary<string, Factory> (ContainsValue): ");
+            TimeDictionaryStringValueLookup(tests, first, lastCopy, middleCopy, custom);
+            Console.WriteLine();
+        }
+
+        public static void TimeQueueTestLookup(TestCollections tests, Factory first, Factory lastCopy, Factory middleCopy, Factory custom)
+        {
+            long ticksFirst = 0;
+            long ticksMiddle = 0;
+            long ticksLast = 0;
+            long ticksCustom = 0;
+
+            bool firstFlag = false;
+            bool middleFlag = false;
+            bool lastFlag = false;
+            bool customFlag = false;
+            for (int i = 0; i < 5; i++)
+            {
+                timer.Start();
+                if (tests.factQueue.Contains(first))
+                {
+                    firstFlag = true;
+                    timer.Stop();
+                    ticksFirst += timer.ElapsedTicks;
+                }
+                timer.Reset();
+
+                timer.Start();
+                if (tests.factQueue.Contains(middleCopy))
+                {
+                    middleFlag = true;  
+                    timer.Stop();
+                    ticksMiddle += timer.ElapsedTicks;
+                }
+                timer.Reset();
+
+                timer.Start();
+                if (tests.factQueue.Contains(lastCopy))
+                {
+                    lastFlag = true;
+                    timer.Stop();
+                    ticksLast += timer.ElapsedTicks;
+                }
+                timer.Reset();
+
+                timer.Start();
+                if (!tests.factQueue.Contains(custom))
+                {
+                    customFlag = true;
+                    timer.Stop();
+                    ticksCustom += timer.ElapsedTicks;
+                }
+                timer.Reset();
+            }
+
+            Console.WriteLine("Тест (первый элемент), время проверки: " + (ticksFirst / 5) + " тиков. Элемент " + (firstFlag ? "найден" : "не найден"));
+            Console.WriteLine($"Тест (средний элемент), время проверки: {ticksMiddle / 5} тиков. Элемент "+ (middleFlag ? "найден" : "не найден"));
+            Console.WriteLine($"Тест (последний элемент), время проверки: {ticksLast / 5} тиков. Элемент "+  (lastFlag ? "найден" : "не найден"));
+            Console.WriteLine($"Тест (незнакомый элемент), время проверки: {ticksCustom / 5} тиков. Элемент "+(customFlag ? "не найден" : "найден"));
+        }
+
+        public static void TimeQueueStringLookup(TestCollections tests, Factory first, Factory lastCopy, Factory middleCopy, Factory custom)
+        {
+            long ticksFirst = 0;
+            long ticksMiddle = 0;
+            long ticksLast = 0;
+            long ticksCustom = 0;
+            bool firstFlag = false;
+            bool middleFlag = false;
+            bool lastFlag = false;
+            bool customFlag = false;
+            for (int i = 0; i < 5; i++) 
+            {
+                timer.Start();
+                if (tests.stringQueue.Contains(first.ToString()))
+                {
+                    firstFlag = true;
+                    timer.Stop();
+                    ticksFirst += timer.ElapsedTicks; 
+                }
+                timer.Reset();
+
+                timer.Start();
+                if (tests.stringQueue.Contains(middleCopy.ToString()))
+                {
+                    middleFlag = true;
+                    timer.Stop();
+                    ticksMiddle += timer.ElapsedTicks;    
+                }
+                timer.Reset();
+
+                timer.Start();
+                if (tests.stringQueue.Contains(lastCopy.ToString()))
+                {
+                    lastFlag = true;
+                    timer.Stop();
+                    ticksLast += timer.ElapsedTicks;
+                }
+                timer.Reset();
+
+                timer.Start();
+                if (!tests.stringQueue.Contains(custom.ToString()))
+                {
+                    customFlag = true;
+                    timer.Stop();
+                    ticksCustom += timer.ElapsedTicks;
+                }
+                timer.Reset();
+            }
+
+            Console.WriteLine("Тест (первый элемент), время проверки: " + (ticksFirst / 5) + " тиков. Элемент " + (firstFlag ? "найден" : "не найден"));
+            Console.WriteLine($"Тест (средний элемент), время проверки: {ticksMiddle / 5} тиков. Элемент " + (middleFlag ? "найден" : "не найден"));
+            Console.WriteLine($"Тест (последний элемент), время проверки: {ticksLast / 5} тиков. Элемент " + (lastFlag ? "найден" : "не найден"));
+            Console.WriteLine($"Тест (незнакомый элемент), время проверки: {ticksCustom / 5} тиков. Элемент " + (customFlag ? "не найден" : "найден"));
+        }
+
+        public static void TimeDictionaryProdKeyLookup(TestCollections tests, Factory first, Factory lastCopy, Factory middleCopy, Factory custom)
+        {
+            long ticksFirst = 0;
+            long ticksMiddle = 0;
+            long ticksLast = 0;
+            long ticksCustom = 0;
+            bool firstFlag = false;
+            bool middleFlag = false;
+            bool lastFlag = false;
+            bool customFlag = false;
+            for (int i = 0; i < 5; i++)
+            {
+                timer.Start();
+                if (tests.prodFactDictionary.ContainsKey(first.BaseProduction))
+                {
+                    firstFlag = true;
+                    timer.Stop();
+                    ticksFirst += timer.ElapsedTicks;
+                    
+                }
+                timer.Reset();
+
+                timer.Start();
+                if (tests.prodFactDictionary.ContainsKey(middleCopy.BaseProduction))
+                {
+                    middleFlag = true;
+                    timer.Stop();
+                    ticksMiddle += timer.ElapsedTicks;
+                    
+                }
+                timer.Reset();
+
+                timer.Start();
+                if (tests.prodFactDictionary.ContainsKey(lastCopy.BaseProduction))
+                {
+                    lastFlag = true;
+                    timer.Stop();
+                    ticksLast += timer.ElapsedTicks;
+
+                }
+                timer.Reset();
+
+                timer.Start();
+                if (!tests.prodFactDictionary.ContainsKey(custom.BaseProduction))
+                {
+                    customFlag = true;
+                    timer.Stop();
+                    ticksCustom += timer.ElapsedTicks;
+                    
+                }
+                timer.Reset();
+            }
+
+            Console.WriteLine("Тест (первый элемент), время проверки: " + (ticksFirst / 5) + " тиков. Элемент " + (firstFlag ? "найден" : "не найден"));
+            Console.WriteLine($"Тест (средний элемент), время проверки: {ticksMiddle / 5} тиков. Элемент " + (middleFlag ? "найден" : "не найден"));
+            Console.WriteLine($"Тест (последний элемент), время проверки: {ticksLast / 5} тиков. Элемент " + (lastFlag ? "найден" : "не найден"));
+            Console.WriteLine($"Тест (незнакомый элемент), время проверки: {ticksCustom / 5} тиков. Элемент " + (customFlag ? "не найден" : "найден"));
+        }
+
+        public static void TimeDictionaryProdValueLookup(TestCollections tests, Factory first, Factory lastCopy, Factory middleCopy, Factory custom)
+        {
+            long ticksFirst = 0;
+            long ticksMiddle = 0;
+            long ticksLast = 0;
+            long ticksCustom = 0;
+            bool firstFlag = false;
+            bool middleFlag = false;
+            bool lastFlag = false;
+            bool customFlag = false;
+            for (int i = 0; i < 5; i++)
+            {
+                timer.Start();
+                if (tests.stringFactDictionary.ContainsValue(first))
+                {
+                    firstFlag = true;
+                    timer.Stop();
+                    ticksFirst += timer.ElapsedTicks;
+                }
+                timer.Reset();
+
+                timer.Start();
+                if (tests.stringFactDictionary.ContainsValue(middleCopy))
+                {
+                    middleFlag = true;
+                    timer.Stop();
+                    ticksMiddle += timer.ElapsedTicks;
+                }
+                timer.Reset();
+
+                timer.Start();
+                if (tests.stringFactDictionary.ContainsValue(lastCopy))
+                {
+                    lastFlag = true;
+                    timer.Stop();
+                    ticksLast += timer.ElapsedTicks;
+                }
+                timer.Reset();
+
+                timer.Start();
+                if (!tests.stringFactDictionary.ContainsValue(custom))
+                {
+                    customFlag = true;
+                    timer.Stop();
+                    ticksCustom += timer.ElapsedTicks;
+                }
+                timer.Reset();
+            }
+
+            Console.WriteLine("Тест (первый элемент), время проверки: " + (ticksFirst / 5) + " тиков. Элемент " + (firstFlag ? "найден" : "не найден"));
+            Console.WriteLine($"Тест (средний элемент), время проверки: {ticksMiddle / 5} тиков. Элемент " + (middleFlag ? "найден" : "не найден"));
+            Console.WriteLine($"Тест (последний элемент), время проверки: {ticksLast / 5} тиков. Элемент " + (lastFlag ? "найден" : "не найден"));
+            Console.WriteLine($"Тест (незнакомый элемент), время проверки: {ticksCustom / 5} тиков. Элемент " + (customFlag ? "не найден" : "найден"));
+        }
+
+        public static void TimeDictionaryStringValueLookup(TestCollections tests, Factory first, Factory lastCopy, Factory middleCopy, Factory custom)
+        {
+            long ticksFirst = 0;
+            long ticksMiddle = 0;
+            long ticksLast = 0;
+            long ticksCustom = 0;
+            bool firstFlag = false;
+            bool middleFlag = false;
+            bool lastFlag = false;
+            bool customFlag = false;
+            for (int i = 0; i < 5; i++)
+            {
+                timer.Start();
+                if (tests.stringFactDictionary.ContainsValue(first))
+                {
+                    firstFlag = true;
+                    timer.Stop();
+                    ticksFirst += timer.ElapsedTicks;
+                }
+                timer.Reset();
+
+                timer.Start();
+                if (tests.stringFactDictionary.ContainsValue(lastCopy))
+                {
+                    lastFlag = true;
+                    timer.Stop();
+                    ticksLast += timer.ElapsedTicks;
+                }
+                timer.Reset();
+
+                timer.Start();
+                if (tests.stringFactDictionary.ContainsValue(middleCopy))
+                {
+                    middleFlag = true;
+                    timer.Stop();
+                    ticksMiddle += timer.ElapsedTicks;
+                }
+                timer.Reset();
+
+                timer.Start();
+                if (!tests.stringFactDictionary.ContainsValue(custom))
+                {
+                    customFlag = true;
+                    timer.Stop();
+                    ticksCustom += timer.ElapsedTicks;
+                }
+                timer.Reset();
+            }
+
+            Console.WriteLine("Тест (первый элемент), время проверки: " + (ticksFirst / 5) + " тиков. Элемент " + (firstFlag ? "найден" : "не найден"));
+            Console.WriteLine($"Тест (средний элемент), время проверки: {ticksMiddle / 5} тиков. Элемент " + (middleFlag ? "найден" : "не найден"));
+            Console.WriteLine($"Тест (последний элемент), время проверки: {ticksLast / 5} тиков. Элемент " + (lastFlag ? "найден" : "не найден"));
+            Console.WriteLine($"Тест (незнакомый элемент), время проверки: {ticksCustom / 5} тиков. Элемент " + (customFlag ? "не найден" : "найден"));
+        }
+
+        public static void TimeDictionaryStringKeyLookup(TestCollections tests, Factory first, Factory lastCopy, Factory middleCopy, Factory custom)
+        {
+            long ticksFirst = 0;
+            long ticksMiddle = 0;
+            long ticksLast = 0;
+            long ticksCustom = 0;
+            bool firstFlag = false;
+            bool middleFlag = false;
+            bool lastFlag = false;
+            bool customFlag = false;
+            for (int i = 0; i < 5; i++)
+            {
+                timer.Start();
+                if (tests.stringFactDictionary.ContainsKey(first.ToString()))
+                {
+                    firstFlag = true;
+                    timer.Stop();
+                    ticksFirst += timer.ElapsedTicks;
+                }
+                timer.Reset();
+
+                timer.Start();
+                if (tests.stringFactDictionary.ContainsKey(lastCopy.ToString()))
+                {
+                    lastFlag = true;
+                    timer.Stop();
+                    ticksLast += timer.ElapsedTicks;
+                }
+                timer.Reset();
+
+                timer.Start();
+                if (tests.stringFactDictionary.ContainsKey(middleCopy.ToString()))
+                {
+                    middleFlag = true;
+                    timer.Stop();
+                    ticksMiddle += timer.ElapsedTicks;
+                }
+                timer.Reset();
+
+                timer.Start();
+                if (!tests.stringFactDictionary.ContainsKey(custom.ToString()))
+                {
+                    customFlag = true;
+                    timer.Stop();
+                    ticksCustom += timer.ElapsedTicks;
+                }
+                timer.Reset();
+            }
+
+            Console.WriteLine("Тест (первый элемент), время проверки: " + (ticksFirst / 5) + " тиков. Элемент " + (firstFlag ? "найден" : "не найден"));
+            Console.WriteLine($"Тест (средний элемент), время проверки: {ticksMiddle / 5} тиков. Элемент " + (middleFlag ? "найден" : "не найден"));
+            Console.WriteLine($"Тест (последний элемент), время проверки: {ticksLast / 5} тиков. Элемент " + (lastFlag ? "найден" : "не найден"));
+            Console.WriteLine($"Тест (незнакомый элемент), время проверки: {ticksCustom / 5} тиков. Элемент " + (customFlag ? "не найден" : "найден"));
+        }
+    }
+}
---
+++ Laba№11/TestCollections.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductionLib;
+
+namespace lab11
+{
+    internal class TestCollections
+    {
+        public Queue<Factory> factQueue = new Queue<Factory>();
+        public Queue<string> stringQueue = new Queue<string>();
+        public Dictionary<Production, Factory> prodFactDictionary = new Dictionary<Production, Factory>();
+        public Dictionary<string, Factory> stringFactDictionary = new Dictionary<string, Factory>();
+
+        public TestCollections() { }
+
+        public TestCollections(int count)
+        {
+            factQueue = new Queue<Factory>();
+            stringQueue = new Queue<string>();
+            prodFactDictionary = new Dictionary<Production, Factory>();
+            stringFactDictionary = new Dictionary<string, Factory>();
+
+            int addedFactoriesCount = 0;
+
+            while (addedFactoriesCount < count)
+            {
+                Factory fact = new Factory();
+                fact.RandInit();
+
+                if (!prodFactDictionary.ContainsKey(fact.BaseProduction) && !stringFactDictionary.ContainsKey(fact.ToString()))
+                {
+                    factQueue.Enqueue(fact);
+                    stringQueue.Enqueue(fact.ToString());
+                    prodFactDictionary.Add(fact.BaseProduction, fact);
+                    stringFactDictionary.Add(fact.ToString(), fact);
+                    addedFactoriesCount++; 
+                }
+                
+            }
+        }
+
+        public Factory GetLastTest()
+        {
+            if (factQueue.Count == 0)
+            {
+                throw new InvalidOperationException("Очередь пуста. Невозможно получить последний элемент.");
+            }
+
+            Factory[] factoryArray = factQueue.ToArray();
+
+            return factoryArray[factoryArray.Length - 1];
+        }
+
+        public Factory GetMiddleTest()
+        {
+            if (factQueue.Count < 500)
+            {
+                throw new InvalidOperationException("В очереди недостаточно элементов. Невозможно получить 500-й элемент.");
+            }
+
+            Factory[] factoryArray = factQueue.ToArray();
+
+            return factoryArray[499];
+        }
+    }
+}
